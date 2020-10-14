017ccf406513f68621df02970d555b4e299e1d56
Nikita <nafomintversu@gmail.com>
2020-10-15 02:49:13 +0300

    implemented lowpass filter

---
+++ lowpassfilter.cpp
@@ -0,0 +1,6 @@
+#include "lowpassfilter.h"
+
+LowPassFilter::LowPassFilter()
+{
+
+}
---
+++ lowpassfilter.h
@@ -0,0 +1,11 @@
+#ifndef LOWPASSFILTER_H
+#define LOWPASSFILTER_H
+
+
+class LowPassFilter
+{
+public:
+    LowPassFilter();
+};
+
+#endif // LOWPASSFILTER_H
